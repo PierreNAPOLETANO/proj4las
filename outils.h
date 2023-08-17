b2388d698221978d3af6c6546d56a1d68691e55a
Pierre NAPOLETANO <56818795+PierreNAPOLETANO@users.noreply.github.com>
2023-08-17 10:29:18 +0200

    Update outils.h
    reindent outils.h to make it easier to read

--- outils.h
+++ outils.h
@@ -19,15 +19,12 @@ class outils
 public:
     outils(){;}
 
+    static std::string ToString(double x);
+    static std::string ToString(unsigned long int x);
+    static std::string ToString(long int x);
 
-  static   std::string ToString( double x);
-  static   std::string ToString( unsigned long int x);
-  static   std::string ToString( long int x);
-
-static inline double DegToRad(double deg) {return deg*std::atan((double)1)*4/180;} 
-static inline double RadToDeg(double deg) {return deg*45/std::atan((double)1);} 
-
-
+    static inline double DegToRad(double deg) { return deg * std::atan((double)1) * 4 / 180; } 
+    static inline double RadToDeg(double deg) { return deg * 45 / std::atan((double)1); }
 };
 
 #endif // OUTILS_H
